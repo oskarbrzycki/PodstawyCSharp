75a7c4028b62c0c8db59bc4eb39f45243b923300
Oskar Brzycki <mroscar005@gmail.com>
2025-03-27 19:05:02 +0100

    ff

--- PodstawyCSharp/Program.cs
+++ PodstawyCSharp/Program.cs
@@ -10,6 +10,12 @@ namespace PodstawyCSharp
     {
         static void Main(string[] args)
         {
+            //Napisać program wypisujący na ekranie kolejne liczby całkowite od 11 do 33. Użyj pętli for.
+            int g;
+            for (int g = 11; g <= 33; g++);
+            Console.Write("{0}", g);
+            Console.ReadKey(true);
+
         }
     }
 }
---
+++ Zadaniee2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadaniee2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Napisać program wczytujący wartość liczby naturalnej n (n>=0), obliczający wartość 2^n (2 do
+            //potęgi n) i wypisujący wynik na ekranie w formacie(np.dla n = 10) : 2 ^ 10 = 1024.Do obliczenia
+            //wartości wyrażenia użyj pętli for.
+            int n;
+            int i; int wynik = 1;
+            Console.WriteLine("Podaj liczbe n");
+            n = int.Parse(Console.ReadLine());
+            for (i = 1; i <= n; i++)
+            {
+                wynik = wynik * 2;
+            }
+            {
+                
+            }
+            Console.Write("2^{0} = {1}", n, wynik);
+            Console.ReadKey(true);
+
+
+        }
+    }
+}
