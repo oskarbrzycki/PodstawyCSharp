51e980fc5b62cf28087bafd035b45fcdf5325c64
oskarbrzycki <mroscar005@gmail.com>
2025-06-10 00:53:55 +0200

    ffssds

---
+++ Zadanie 2 struktury/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_2_struktury
+{
+    internal class Program
+    {
+        //definicja struktury
+        struct Uczen
+        {
+            public string imie, nazwisko;
+            public byte[] oceny;
+        }
+
+       
+        
+            static void Main(string[] args)
+            {
+                Uczen[] uczniowie; //deklaruję zmienną typu strukturalnego
+                int ilu_uczniow, ile_ocen, i, j; //zmienne pomocnicze
+
+                //wczytuję liczbę uczniów
+                Console.Write("Podaj liczbę uczniów w klasie: ");
+                ilu_uczniow = Convert.ToInt32(Console.ReadLine());
+
+                //przydzielam pamięć dla tablicy struktur
+                uczniowie = new Uczen[ilu_uczniow];
+
+                //wczytuję dane uczniów od użytkownika
+                for (i = 0; i < ilu_uczniow; i++)
+                {
+                    Console.Write("Podaj imię ucznia {0}: ", i + 1);
+                    uczniowie[i].imie = Console.ReadLine();
+
+                    Console.Write("Podaj nazwisko ucznia {0}: ", i + 1);
+                    uczniowie[i].nazwisko = Console.ReadLine();
+
+                    //wczytuję liczbę ocen ucznia
+                    Console.Write("Ile ocen ma {0} {1}: ", uczniowie[i].imie, uczniowie[i].nazwisko);
+                    ile_ocen = Convert.ToInt32(Console.ReadLine());
+                //przydzielam pamięć dla tablicy ocen
+                uczniowie[i].oceny = new byte[ile_ocen];
+
+                //wczytuję oceny ucznia
+                for (j = 0; j < ile_ocen; j++)
+                {
+                    Console.Write("Podaj ocenę numer {0}: ", j + 1);
+                    uczniowie[i].oceny[j] = Convert.ToByte(Console.ReadLine());
+                }
+
+                Console.WriteLine(); //wypisanie pustej linii
+                for (i = 0; i < ilu_uczniow; i++)
+                {
+                    Console.Write("{0}. {1} {2}: ",
+                        i + 1, uczniowie[i].imie, uczniowie[i].nazwisko);
+
+                    //wypisuję oceny ucznia
+                    foreach (byte o in uczniowie[i].oceny)
+                        Console.Write("{0} ", o);
+
+                    Console.WriteLine(""); //przejście do nowego wiersza
+                }
+
+                Console.ReadKey(true); //pauza
+            }
+            }
+        }
+    }
+
---
+++ Zadanie 3 struktury/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_3_struktury
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                int N = 10; //maksymalna ilość elementów tablicy
+                string[] owoce = new string[N];
+                int ile = 0; //ilość elementów w tablicy
+                int w, nr, i; //wybór; numer elementu tablicy; licznik pętli
+
+                do
+                {
+                    Console.WriteLine("\nMENU:\n1-dodaj\n2-modyfikuj\n3-usuń");
+                    Console.Write("4-wypisz\n0-koniec\nTwój wybór: ");
+                    w = Convert.ToInt32(Console.ReadLine());
+
+                    switch (w)
+                    {
+                        case 0: break;
+
+                        case 1: //dodaj owoc
+                            Console.Write("Podaj nazwę owocu: ");
+                            owoce[ile++] = Console.ReadLine();
+                            break;
+
+                        case 2: //modyfikuj nazwę owocu
+                            Console.Write("Podaj numer owocu do modyfikacji: ");
+                            nr = Convert.ToInt32(Console.ReadLine());
+                            if ((nr <= ile) && (nr > 0))
+                            {
+                                Console.WriteLine("Obecna nazwa owocu: {0}", owoce[nr - 1]);
+                                Console.Write("Podaj nową nazwę owocu: ");
+                                owoce[nr - 1] = Console.ReadLine();
+                            }
+                            else Console.WriteLine("Błędny numer !!!");
+                            break;
+                        case 3: //usuń owoc
+                            if (ile == 0) Console.WriteLine("Brak elementów !!!");
+                            else
+                            {
+                            }
+                            Console.Write("Podaj numer owocu do usunięcia: "); nr = Convert.ToInt32(Console.ReadLine()); if ((nr <= ile) && (nr > 0))
+                            {
+                                Console.WriteLine("Usunięty owoc: {0}", owoce[nr - 1]); if (ile == 1) ile = 0;
+                                else
+                                {
+                                }
+                                for (i - nr - 1; i < ile - 1; i++) owoce[i] = owoce[i + 1]; ile--;
+                            }
+                            else Console.WriteLine("Błędny numer !!!");
+                            break;
+                        case 4: //wypisz wszystkie owoce
+                            Console.WriteLine("Wpisane owoce:");
+                            for (i = 0; i < ile; i++)
+                                Console.WriteLine("{0}. {1}", i + 1, owoce[i]);
+                            break;
+                        default: Console.WriteLine("Nieznany wybor !!!"); break;
+                    } while (w != 0) ;
+                    Console.ReadKey(true); //pauza
+
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+
